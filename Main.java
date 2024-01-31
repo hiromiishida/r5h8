610fa16955088c3858c3638d448b69c0171338d5
ishida <digitalplan21@gmail.com>
2024-01-31 10:55:16 +0900

    ”テスト６”を出力する

--- Main.java
+++ Main.java
@@ -1,5 +1,3 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("てすと５");
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:ec28c8bc096cd61ee2f18ae8611b46b4e9227e4a3dc205e1b9f167a1620790c2
+size 120
